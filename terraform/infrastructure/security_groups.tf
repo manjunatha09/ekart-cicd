9c78e03b5603cc37090ab01e4972ebf7db23e331
manjunatha09 <manjuc.28.06@gmail.com>
2026-09-05 14:09:12 +0530

    Update security_groups.tf

--- terraform/infrastructure/security_groups.tf
+++ terraform/infrastructure/security_groups.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "jenkins" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.admin_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -41,7 +41,7 @@ resource "aws_security_group" "sonarqube" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.admin_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -73,7 +73,7 @@ resource "aws_security_group" "k8s" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.admin_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
